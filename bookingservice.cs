c8ce2c306b2e071cd7a1421886957da95990600f
SarathSK24 <sarath2924@gmail.com>
2025-06-17 08:03:03 +0530

    Add files via upload

---
+++ bookingservice.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppSmartAppointmentBooking
+{
+    public class bookingservices : Ibookingservice
+    {
+        List<Appointment> _appointments;
+        public bookingservices(List<Appointment> appointments)
+        {
+            _appointments = appointments;
+        }
+        public IEnumerable<Appointment> GetAppointmentsByDate(DateTime date) 
+        {
+            return _appointments.Where (a => a.Date.Date == date);
+
+        }
+        public IEnumerable<Appointment> GetAppointmentsByUser(string user)
+        {
+            return _appointments.Where(a => a.user.Equals(user, StringComparison.OrdinalIgnoreCase));
+        }
+        public IEnumerable<Appointment> GetAppointmentsByProfession(string Professional)
+        {
+            return _appointments.Where(a => a.Professional.Equals(Professional, StringComparison.OrdinalIgnoreCase));
+        }
+
+    }
+}
